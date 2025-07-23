88e1659586d7c2b9330e3f2968a047fd2b596a16
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-07-23 14:57:25 -0300

    1.5
    Footer Completo com informações da empresa e do desenvolvedor

--- Geovana_Personalização/src/components/Footer.jsx
+++ Geovana_Personalização/src/components/Footer.jsx
@@ -1,36 +1,44 @@
 import styles from "../components/modules/footer.module.css"
+import vetraLogo from "../assets/icons/vetra-logo.png"
+import instaLogo from "../assets/icons/insta-logo.png"
+import faceLogo from "../assets/icons/face-logo.png"
+import linkedinLogo from "../assets/icons/linkedin-logo.png"
+import telefoneLogo from "../assets/icons/telefone-logo.png"
+import emailLogo from "../assets/icons/gmail-logo.png"
+import gitLogo from "../assets/icons/github-logo.png"
+
 
 function Footer() {
     return (
         <footer>
         <div className={styles.sobre_empresa}>
-            <div>
-                <p>Explorar Páginas</p>
+            <div className={styles.div_info}>
+                <h2>Explorar Páginas</h2>
                 <ul>
                     <li><p>Galeria</p></li>
                     <li><p>Como Funciona?</p></li>
                 </ul>
             </div>
-            <div>
-                <p>Contato:</p>
-                <span><img src="" alt="Telefone" /><p>(11)93342-0745</p></span>
-                <span><img src="" alt="Gmail" /><p>thwo28391@gmail.com</p></span>
+            <div className={`${styles.div_info} ${styles.contato}`}>
+                <h2>Contato:</h2>
+                <p><img src={telefoneLogo} alt="Telefone" />(11)xxxxx-xxxx</p>
+                <p><img src={emailLogo} alt="email" />thwo28391@gmail.com</p>
             </div>
-            <div>
-                <p>Redes Sociais:</p>
+            <div className={`${styles.div_info} ${styles.redes_sociais}`}>
+                <h2>Redes Sociais:</h2>
                 <div className={styles.redes_da_empresa}>
-                    <img src="" alt="Instagram" />
-                    <img src="" alt="Facebook" />
+                    <img src={instaLogo} alt="Instagram" />
+                    <img src={faceLogo} alt="Facebook" />
                 </div>
             </div>
         </div>
         <div className={styles.sobre_desenvolvedor}>
             <p>Site Desenvolvido por Thiago Santos&copy;</p>
-            <img src="" alt="Vetra Logo" />
-            <div>
-                <img src="" alt="Instagram" />
-                <img src="" alt="Linkedin" />
-                <img src="" alt="Gihub" />
+            <img src={vetraLogo} alt="Vetra Logo" />
+            <div className={styles.redes_dev}>
+                <img src={instaLogo} alt="Instagram" />
+                <img src={linkedinLogo} alt="Linkedin" />
+                <img src={gitLogo} alt="Gihub" />
             </div>
         </div>
     </footer>
